d46c97e3e83a5adfcde553c5545791a95db88719
kazuyoshi80 <kkawakami@smartshopping.co.jp>
2025-01-28 16:03:28 +0900

    Refactor color retrieval to use dynamic image value.
    Replaced the hardcoded image URL with a dynamic value from `image.value`. This ensures flexibility and prevents dependence on a static resource. Error handling remains unchanged for robustness.

--- src/components/Clips/Clip.vue
+++ src/components/Clips/Clip.vue
@@ -139,12 +139,10 @@ onMounted(async () => {
 
 	if (SKIN.includes(tag.value)) {
 		const fac = new FastAverageColor()
-		color.value = await fac
-			.getColorAsync('https://storage.clubs.place/3zS5PXdjvLHgP7p1aAV7q')
-			.catch((e) => {
-				console.error(e)
-				return undefined
-			})
+		color.value = await fac.getColorAsync(image.value || '').catch((e) => {
+			console.error(e)
+			return undefined
+		})
 	}
 	await updateImageIfNeeded()
 })
